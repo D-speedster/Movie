0c6b44268777a7a44f57f198a010ad7ed0be09ac
D-speedster <mrspeed717@gmail.com>
2023-06-05 11:54:58 +0330

    Update Build

---
+++ build/precache-manifest.cf6c36196d46fdb5c60add7f85951eff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae38d6f7396a0954caa4b8128ed1f67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a037d600966efc0facef",
+    "url": "/static/css/2.777d1846.chunk.css"
+  },
+  {
+    "revision": "1aea4f65a9de227bcaac",
+    "url": "/static/css/main.6d9c541c.chunk.css"
+  },
+  {
+    "revision": "a037d600966efc0facef",
+    "url": "/static/js/2.6c52ad26.chunk.js"
+  },
+  {
+    "revision": "6f5c27c1ea8c1092e8a1b2a7e7d4497f",
+    "url": "/static/js/2.6c52ad26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aea4f65a9de227bcaac",
+    "url": "/static/js/main.1f15281d.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.1f15281d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fa959f9ec424b9630fc",
+    "url": "/static/js/runtime-main.5d5b8f7a.js"
+  }
+]);
