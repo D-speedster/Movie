6caf61615e57d060b756cb241f244fb8fafd9f62
D-speedster <mrspeed717@gmail.com>
2023-08-31 18:14:04 +0430

    update url2

--- src/Services/Axios/config.js
+++ src/Services/Axios/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const ApiRequest = axios.create({
-    baseURL: " database-kappa-five.vercel.app/",
+    baseURL: " https://database-kappa-five.vercel.app/",
     headers: {
         'Content-Type': 'application/json'
     }
