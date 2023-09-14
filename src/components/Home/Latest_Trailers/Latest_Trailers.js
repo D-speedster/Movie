5afdaef39b8f1899adb9d6f83ab33d89511d0a4b
D-speedster <mrspeed717@gmail.com>
2023-09-14 18:35:22 +0330

    change structure last trailer

--- src/components/Home/Latest_Trailers/Latest_Trailers.js
+++ src/components/Home/Latest_Trailers/Latest_Trailers.js
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Col, Row, Container } from 'react-bootstrap'
 import './Latest_Trailers.css';
 import { BsPlayCircleFill } from 'react-icons/bs'
 import { HiVideoCamera } from 'react-icons/hi'
 import Boxoffice from '../Boxofiice/Boxoffice';
-// const images = require.context("/img/Last_Trailer/", true)
-// const defaultImage = images.keys()[0].split('/')[1];
-// console.log(`./img/${defaultImage}`)
 
 export default function Latest_Trailers(props) {
+  let [countImg, SetcountImg] = useState(0)
   let ChangeBackTrailer = (event) => {
     console.log(event.target.dataset.img)
     console.log(event.target.parentNode.parentNode.parentNode.style.background = `url(${event.target.dataset.img})`)
     console.log(event.target.parentElement)
   }
+  let last_trailer = [
+    { id: 1, img: 'img/Last_Trailer/Trailer_Background.jpg' },
+    { id: 2, img: 'img/Last_Trailer/t_poster3.jpg' },
+    { id: 3, img: 'img/Last_Trailer/t_poster2.jpg' },
+    { id: 4, img: 'img/Last_Trailer/t_poster1.jpg' }
 
+  ]
+  useEffect(() => {
+    const interval = setInterval(() => {
+      let counter = countImg + 1;
+      if (counter === 3) {
+        counter = 0;
+      }
+      SetcountImg(counter)
+      console.log(countImg)
+    }, 5500);
+  
+    return () => clearInterval(interval);
+  }, [countImg]);
 
   return (
     <Container>
@@ -26,26 +42,27 @@ export default function Latest_Trailers(props) {
           </h4>
           <div className='Latest_Trailers'>
 
-            <div className='Latest_Trailers_Box' style={{ background: `url(/img/Last_Trailer/Trailer_Background.jpg)` }}>
+            <div className='Latest_Trailers_Box' style={{ background: `url(${last_trailer[countImg].img})` }}>
 
-              <Row>
-                <Col lg={6} md={6} sm={6} xs={6} className='Last_Trailer_Item'>
-                  <img alt="Trailer_Background" src='/img/Last_Trailer/Trailer_Background.jpg' data-img='/img/Last_Trailer/Trailer_Background.jpg' onMouseOverCapture={ChangeBackTrailer}></img>
-                  <BsPlayCircleFill></BsPlayCircleFill>
-                </Col>
-                <Col lg={6} md={6} sm={6} xs={6} className='Last_Trailer_Item'>
-                  <img alt="t_poster1" src='/img/Last_Trailer/t_poster1.jpg' data-img='/img/Last_Trailer/t_poster1.jpg' onMouseOverCapture={ChangeBackTrailer}></img>
-                  <BsPlayCircleFill></BsPlayCircleFill>
-                </Col>
-                <Col lg={6} md={6} sm={6} xs={6} className='Last_Trailer_Item'>
-                  <img alt="t_poster2" src='/img/Last_Trailer/t_poster2.jpg' data-img='/img/Last_Trailer/t_poster2.jpg' onMouseOverCapture={ChangeBackTrailer}></img>
-                  <BsPlayCircleFill></BsPlayCircleFill>
-                </Col>
-                <Col lg={6} md={6} sm={6} xs={6} className='Last_Trailer_Item'>
-                  <img alt="t_poster3" src='/img/Last_Trailer/t_poster3.jpg' data-img='./img/Last_Trailer/t_poster3.jpg' onMouseOverCapture={ChangeBackTrailer}></img>
-                  <BsPlayCircleFill></BsPlayCircleFill>
-                </Col>
-              </Row>
+
+              <div className='pagin_img'>
+                <div className='pagin_img_item'>
+                  <img src={last_trailer['1'].img} data-img={last_trailer['1'].img} onMouseEnter={ChangeBackTrailer} />
+                </div>
+                <div className='pagin_img_item mt-2' >
+                  <img src={last_trailer['2'].img} data-img={last_trailer['2'].img} onMouseEnter={ChangeBackTrailer} />
+                </div>
+                <div className='pagin_img_item mt-2'>
+                  <img src={last_trailer['3'].img} data-img={last_trailer['3'].img} onMouseEnter={ChangeBackTrailer} />
+                </div>
+                <div className='pagin_img_item mt-2' >
+                  <img src={last_trailer['0'].img} data-img={last_trailer['0'].img} onMouseEnter={ChangeBackTrailer} />
+                </div>
+
+              </div>
+              <div className='play-circle'>
+                <BsPlayCircleFill></BsPlayCircleFill>
+              </div>
 
             </div>
           </div>
