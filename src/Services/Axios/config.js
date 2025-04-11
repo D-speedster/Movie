d6c25a6b720bc5de844c1c8d4ac9a6cca2244849
D-speedster <116084778+D-speedster@users.noreply.github.com>
2025-04-11 11:24:12 +0330

    Update config.js

--- src/Services/Axios/config.js
+++ src/Services/Axios/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const ApiRequest = axios.create({
-    baseURL: " https://dev-speedster.ir/",
+    baseURL: "https://database-kappa-five.vercel.app/",
     headers: {
         'Content-Type': 'application/json' ,
 
